4d6412d22f74a4a6e985d01f6b691009235a5025
Sanjay Sukhwani <sanjayasukhwani@gmail.com>
2024-07-22 13:13:40 +0530

    Update : Removed the Username Validation From the Signup Schema

--- src/zod-schemas/signUpSchema.ts
+++ src/zod-schemas/signUpSchema.ts
@@ -1,12 +1,8 @@
 // Importing Zod
 import { z } from "zod";
 
-// Username Validation using zod
-const usernameValidation = z
-    .string()
-    .min(2, `Username must be atleast 2 characters`)
-    .max(20, `Username must be not more than 20 characters`)
-    .regex(/^[a-zA-Z0-9-_]+$/, "Username must not contain special Characters")
+// Importing the Username Validation using zod from file
+import { usernameValidation } from "./validationSchemas";
 
 // Exporting the Signup Schema, as it will contain many fields, so we will define object
 export const signUpSchema = z.object({
