3cb1c00540118f20e609a9ee3c6378238bbf4e2f
Sanjay Sukhwani <sanjayasukhwani@gmail.com>
2024-07-24 19:29:00 +0530

    Update : Added the Navbar that we have created in the components that was using session

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import AuthProvider from "@/context/AuthProvider";
 import { Toaster } from "@/components/ui/toaster";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,11 +23,14 @@ export default function RootLayout({
             {/* Wrapping the body inside the SessionProvider as we have to use NextAuth Authentication */}
             <AuthProvider>
                 <body className={inter.className}>
-                    {children}
+
+                    <Navbar />
                     
+                    {children}
+
                     {/* Importing the shadcn Toaster for showing Messages */}
                     <Toaster />
-                    
+
                 </body>
             </AuthProvider>
 
